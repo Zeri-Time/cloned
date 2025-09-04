497b3b21ebdecc67e7eac31ec3a9e967f08dc20b
sik2 <bickwang123@gmail.com>
2025-09-04 10:45:21 +0900

    16-2

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,6 +3,7 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,11 +40,11 @@ public class ApiV1PostController {
 
     @Transactional
     @GetMapping("/{id}/delete")
-    public PostDto delete(@PathVariable Long id) {
+    public RsData delete(@PathVariable Long id) {
         Post post = postService.findById(id);
 
         postService.delete(post);
 
-        return new PostDto(post);
+        return new RsData("200-1", "%d 번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
     }
 }
