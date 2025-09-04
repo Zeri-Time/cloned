83f010ed0a27a1627a69b33ee0093f4e5a0079be
sik2 <bickwang123@gmail.com>
2025-09-04 12:06:50 +0900

    21

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -6,10 +6,7 @@ import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,7 +36,7 @@ public class ApiV1PostController {
     }
 
     @Transactional
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public RsData<PostDto> delete(@PathVariable Long id) {
         Post post = postService.findById(id);
 
