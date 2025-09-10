060d8c096adfa4d8afd88448968926b689c9e8af
sik2 <bickwang123@gmail.com>
2025-09-10 14:43:25 +0900

    62-1

--- src/main/java/com/back/domain/home/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/home/controller/HomeController.java
@@ -30,4 +30,21 @@ public class HomeController {
                 </div>
                 """.formatted(localHost.getHostName(), localHost.getHostAddress());
     }
+
+    @GetMapping(value="/test/fetchPosts", produces = TEXT_HTML_VALUE)
+    public String testFetchPosts() {
+
+        return """
+                    <script>
+                    console.clear();
+    
+                    fetch("http://localhost:8080/api/v1/posts")
+                      .then(response => response.json())
+                      .then(data => {
+                        console.log(data);
+                        console.log(data[1].title);
+                      });
+                    </script>
+                """;
+    }
 }
